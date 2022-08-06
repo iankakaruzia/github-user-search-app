70b486fddeaec4aa1c97f8e39fb4656a1a7ad01c
Ianka Karuzia <iankakaruzia@gmail.com>
2022-08-05 23:39:51 -0300

    feat: request default user info on the server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
 import type { NextPage } from 'next'
+import { api } from 'services/api'
 
-const Home: NextPage = () => {
+type User = {
+  id: number
+  name: string | null
+  login: string
+  bio: string | null
+  avatar_url: string
+  created_at: string
+  public_repos: number
+  followers: number
+  following: number
+  location: string | null
+  blog: string | null
+  twitter_username: string | null
+  company: string | null
+}
+
+type Props = {
+  defaultUser: User
+}
+
+const Home: NextPage<Props> = ({ defaultUser }) => {
   return (
     <div>
       <h1 className='text-base'>Hello</h1>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const { data } = await api.get<User>('users/octocat')
+
+  return {
+    props: {
+      defaultUser: data
+    }
+  }
 }
 
 export default Home
